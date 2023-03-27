f65795eff2dd4cbfb7d60738fbea704e54d16cfa
daehan <daehanl8635@gmail.com>
2023-03-28 01:04:54 +0900

    회원가입,로그인,마이페이지까지

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'))
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 var db;
-MongoClient.connect('mongodb+srv://admin:@cluster0.0sp7tde.mongodb.net/?retryWrites=true&w=majority', function (error, client) {
+MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.0sp7tde.mongodb.net/?retryWrites=true&w=majority', function (error, client) {
     if (error) { return console.log(error) };
 
     db = client.db('FootballMatchingapp');
@@ -23,70 +30,27 @@ app.get('/', function (req, res) {
     res.render('index.ejs');
 })
 
-app.get('/mypage', function (req, res) {
-    res.render('mypage.ejs');
-});
-
 app.get('/editprofile', function (req, res) {
     res.render('editprofile.ejs');
 });
 
-// app.get('/editprofile/:id', function(req, res) {
-//     db.collection('profile').findOne({_id : parseInt(req.params.id)}, function(에러, 결과){
-//         if(에러){ return 에러};
-//         console.log(결과);
-//         res.render('editprofile.ejs', {profile : result});
-//     })
-
-// });
-
-//작성한 프로필 db에 저장
-app.post('/editprofile', function (req, res) {
-    db.collection('counter').findOne({ name: '프로필' }, function (error, result) {
-        console.log(result.total);
-        var Profile = result.total;
-        db.collection('profile').insertOne({
-            _id: Profile,
-            닉네임: req.body.nickname,
-            성별: req.body.gender,
-            선호포지션: req.body.position,
-            주발: req.body.foot,
-            신장: req.body.height,
-            몸무게: req.body.weight
-        }, function (error, result) {
-            db.collection('counter').updateOne({ name: '프로필' }, { $inc: { total: 1 } }, function (error, result) {
-                if (error) { return console.log(error) };
-            });
-            console.log('저장완료');
-        });
-    });
-    //성공 알림창 띄우고 마이페이지로 돌아가게함 
-    res.write("<script>alert('success')</script>");
-    res.write("<script>window.location=\"../mypage\"</script>");
-    console.log(req.body.nickname);
-});
-
-//내 프로필 보기
-app.get('/myprofile', function (req, res) {
-    db.collection('profile').find().toArray(function (error, result) {
-        console.log(result);
-        res.render('myprofile.ejs', { profile: result });
-    });
-});
-
 //프로필 수정하기
-app.get('/editprofile/:id', function (req, res) {
-    db.collection('profile').findOne({ _id: parseInt(req.params.id) }, function (error, result) {
-        if (error) { return error };
+app.get('/editprofile', function (req, res) {
+    console.log(req.user);
+    db.collection('profile').findOne({ _id: req.user._id }, function (error, result) {
         console.log(result);
-        res.render('editprofile.ejs', { profile: result });
+        if (error) { return error };
+
+        res.render('editprofile.ejs');
     })
 
 })
+//프로필 수정
 app.put('/editprofile', function (req, res) {
     // 폼에담긴 제목데이터, 날짜데이터를 가지고 
     // db.collection에다가 업데이트함
-    db.collection('profile').updateOne({ _id: parseInt(req.body.id) }, {
+    // console.log(req.user);
+    db.collection('profile').updateOne({ _id: req.user._id }, {
         $set:
         {
             닉네임: req.body.nickname,
@@ -97,10 +61,129 @@ app.put('/editprofile', function (req, res) {
             몸무게: req.body.weight
         }
     }, function (error, result) {
-        console.log('수정완료');
-        res.redirect('/myprofile');//요청 성공시 페이지이동하게 해주는 코드
-        //응답은 필수임 안하면 서버가 멈춤
+        // console.log('수정완료');
+        res.write("<script>alert('success')</script>");
+        res.write("<script>window.location=\"../myprofile\"</script>");
 
     })
 
 })
+
+// 회원가입
+app.get('/register', function(req, res) {
+    res.render('register.ejs');
+});
+
+app.post('/register', function (req, res) {
+
+    db.collection('profile').findOne({ id: req.body.id }, function (error, result) {
+        if (result) {
+            res.send('아이디 중복'); //아이디 중복
+        } else {
+            db.collection('profile').insertOne({
+                id: req.body.id,
+                pw: req.body.pw
+            }, function (error, result) {
+                if (error) { return error };
+                res.write("<script>alert('success')</script>");
+                res.write("<script>window.location=\"../login\"</script>");
+            })
+        }
+    })
+});
+
+//로그인
+app.get('/login', function(req, res){
+    res.render('login.ejs');
+})
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect : '/fail'
+}), function(req, res){
+    res.redirect('/')
+});
+
+app.get('/fail', function(req, res) {
+    res.render('fail.ejs');
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+  }, function (inputid, inputpw, done) {
+    db.collection('profile').findOne({ id: inputid }, function (error, result) {
+      if (error) return done(error)
+  
+      if (!result) return done(null, false, { message: '존재하지않는 아이디입니다.' })
+      if (inputpw == result.pw) {
+        return done(null, result)
+      } else {
+        return done(null, false, { message: '비밀번호가 일치하지 않습니다.' })
+      }
+    })
+  }));
+
+  //로그인 성공시 id 이용해서 세션을 저장시킴
+  passport.serializeUser(function (user, done) {
+    done(null, user.id)
+  });
+  
+  passport.deserializeUser(function (id, done) {
+    db.collection('profile').findOne({id: id}, function(error, result) {
+        if (error) { return error };
+
+        done(null, result)
+    })
+  });
+
+  app.get('/mypage', checklogin, function(req, res) {
+    // console.log(req.user);
+    res.render('mypage.ejs', {User : req.user});
+  })
+
+  function checklogin(req, res, next){
+    if (req.user) {
+        next();
+    } else {
+        res.send('로그인이 필요합니다.');
+    }
+  }
+
+  //프로필 입력
+  app.get('/inputprofile', checklogin, function(req, res){
+    res.render('inputprofile.ejs');
+  });
+
+  app.post('/inputprofile', function (req, res) {
+    // console.log(req.user);
+
+    db.collection('profile').findOne({_id: req.user._id}, function(error, result){
+        db.collection('profile').updateOne({_id: req.user._id}, {
+        $set:
+        {
+            닉네임: req.body.nickname,
+            성별: req.body.gender,
+            선호포지션: req.body.position,
+            주발: req.body.foot,
+            신장: req.body.height,
+            몸무게: req.body.weight
+        }
+        }, function (error, result) {console.log('저장완료');});
+    });
+
+    //성공 알림창 띄우고 마이페이지로 돌아가게함 
+    res.write("<script>alert('success')</script>");
+    res.write("<script>window.location=\"../mypage\"</script>");
+});
+
+//내 프로필 보기
+app.get('/myprofile', function (req, res) {
+    // console.log(req.user);
+
+    db.collection('profile').findOne({ _id: req.user._id }, function (error, result) {
+        res.render('myprofile.ejs', { User: result });
+    });
+});
+
