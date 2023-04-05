8a724469d9be7afc89ac49f353bf0ad536c0f498
나정배 <najungbae56@naver.com>
2023-04-05 16:10:34 +0900

    게시글 입력 조회기능까지 함

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'))
-app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(session({ secret: '비밀코드', resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -70,7 +70,7 @@ app.put('/editprofile', function (req, res) {
 })
 
 // 회원가입
-app.get('/register', function(req, res) {
+app.get('/register', function (req, res) {
     res.render('register.ejs');
 });
 
@@ -89,21 +89,25 @@ app.post('/register', function (req, res) {
                 res.write("<script>window.location=\"../login\"</script>");
             })
         }
+
+        db.collection('board').insertOne({id: req.body.id}, function(error, result){
+            res.write("<script>window.location=\"../login\"</script>");
+        })
     })
 });
 
 //로그인
-app.get('/login', function(req, res){
+app.get('/login', function (req, res) {
     res.render('login.ejs');
 })
 
 app.post('/login', passport.authenticate('local', {
-    failureRedirect : '/fail'
-}), function(req, res){
+    failureRedirect: '/fail'
+}), function (req, res) {
     res.redirect('/')
 });
 
-app.get('/fail', function(req, res) {
+app.get('/fail', function (req, res) {
     res.render('fail.ejs');
 });
 
@@ -112,65 +116,65 @@ passport.use(new LocalStrategy({
     passwordField: 'pw',
     session: true,
     passReqToCallback: false,
-  }, function (inputid, inputpw, done) {
+}, function (inputid, inputpw, done) {
     db.collection('profile').findOne({ id: inputid }, function (error, result) {
-      if (error) return done(error)
-  
-      if (!result) return done(null, false, { message: '존재하지않는 아이디입니다.' })
-      if (inputpw == result.pw) {
-        return done(null, result)
-      } else {
-        return done(null, false, { message: '비밀번호가 일치하지 않습니다.' })
-      }
-    })
-  }));
+        if (error) return done(error)
 
-  //로그인 성공시 id 이용해서 세션을 저장시킴
-  passport.serializeUser(function (user, done) {
+        if (!result) return done(null, false, { message: '존재하지않는 아이디입니다.' })
+        if (inputpw == result.pw) {
+            return done(null, result)
+        } else {
+            return done(null, false, { message: '비밀번호가 일치하지 않습니다.' })
+        }
+    })
+}));
+
+//로그인 성공시 id 이용해서 세션을 저장시킴
+passport.serializeUser(function (user, done) {
     done(null, user.id)
-  });
-  
-  passport.deserializeUser(function (id, done) {
-    db.collection('profile').findOne({id: id}, function(error, result) {
+});
+
+passport.deserializeUser(function (id, done) {
+    db.collection('profile').findOne({ id: id }, function (error, result) {
         if (error) { return error };
 
         done(null, result)
     })
-  });
+});
 
-  app.get('/mypage', checklogin, function(req, res) {
+app.get('/mypage', checklogin, function (req, res) {
     // console.log(req.user);
-    res.render('mypage.ejs', {User : req.user});
-  })
+    res.render('mypage.ejs', { User: req.user });
+})
 
-  function checklogin(req, res, next){
+function checklogin(req, res, next) {
     if (req.user) {
         next();
     } else {
         res.send('로그인이 필요합니다.');
     }
-  }
+}
 
-  //프로필 입력
-  app.get('/inputprofile', checklogin, function(req, res){
+//프로필 입력
+app.get('/inputprofile', checklogin, function (req, res) {
     res.render('inputprofile.ejs');
-  });
+});
 
-  app.post('/inputprofile', function (req, res) {
+app.post('/inputprofile', function (req, res) {
     // console.log(req.user);
 
-    db.collection('profile').findOne({_id: req.user._id}, function(error, result){
-        db.collection('profile').updateOne({_id: req.user._id}, {
-        $set:
-        {
-            닉네임: req.body.nickname,
-            성별: req.body.gender,
-            선호포지션: req.body.position,
-            주발: req.body.foot,
-            신장: req.body.height,
-            몸무게: req.body.weight
-        }
-        }, function (error, result) {console.log('저장완료');});
+    db.collection('profile').findOne({ _id: req.user._id }, function (error, result) {
+        db.collection('profile').updateOne({ _id: req.user._id }, {
+            $set:
+            {
+                닉네임: req.body.nickname,
+                성별: req.body.gender,
+                선호포지션: req.body.position,
+                주발: req.body.foot,
+                신장: req.body.height,
+                몸무게: req.body.weight
+            }
+        }, function (error, result) { console.log('저장완료'); });
     });
 
     //성공 알림창 띄우고 마이페이지로 돌아가게함 
@@ -183,7 +187,50 @@ app.get('/myprofile', function (req, res) {
     // console.log(req.user);
 
     db.collection('profile').findOne({ _id: req.user._id }, function (error, result) {
-        res.render('myprofile.ejs', { User: result });
+        res.render('myprofile.ejs', { User : result});
     });
 });
+
+
+
+
+
+
+//게시판(board), 등록된 게시글 다 보이게 하는 기능
+app.get('/board', function (req, res) {
+    db.collection('board').find().toArray(function (error, result) {
+        // 디비에 저장된 profile이라는 
+        //collection 안의 모든 데이터를 꺼내주세요
+        res.render('board.ejs', { User : result });
+        //찾은걸 ejs파일에 집어넣어주세요.
+    });
+})
+
+
+//게시물 등록기능
+//게시물을 검색한다. 검색한 내용에 맞는 경기를 DB에서 불러와 보여준다.
+
+//게시글 등록기능
+//게시글 작성하기 버튼을 만들어 게시글 등록 페이지로 이동,게시글을 작성해 자신의 프로필에 경기 내용을 저장한다.
+app.get('/boardinput', function (req, res) {
+    res.render('boardinput.ejs');
+});
+
+app.post('/boardinput', checklogin, function (req, res) {
+    // console.log(req.user);
+
+    db.collection('profile').findOne({ id: req.user.id }, function (error, result) {
+        db.collection('board').updateOne({ id: req.user.id }, {
+            $set:
+            {
+                게시글: req.body.board
+            }
+        }, function (error, result) { console.log('저장완료'); });
+    });
+
+    //성공 알림창 띄우고 마이페이지로 돌아가게함 
+    res.write("<script>alert('success')</script>");
+    res.write("<script>window.location=\"../board\"</script>");
+});
+
 
